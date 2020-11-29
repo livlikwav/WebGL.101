91f8954d26d0e53c3c590fdbbd66d5e698590905
Gyeongmin Ha <gaonrudal@gmail.com>
2020-11-29 16:57:32 +0900

    Improve color scaling

--- geojson/app.js
+++ geojson/app.js
@@ -6,26 +6,45 @@ import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
 import {LightingEffect, AmbientLight, _SunLight as SunLight} from '@deck.gl/core';
 //import {scaleThreshold} from 'd3-scale';
 import {scaleSequential} from 'd3-scale';
-//import {interpolateRainbow} from 'd3-scale-chromatic';
-// import {interpolateOrRd} from 'd3-scale-chromatic';
-// import {interpolateOranges} from 'd3-scale-chromatic';
+import {interpolateOrRd} from 'd3-scale-chromatic';
 import {interpolateBlues} from 'd3-scale-chromatic';
 import {readString} from "react-papaparse";
 
 // "MapboxAccessToken" 환경변수값
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; 
 
-export const COLOR_SCALE = x =>
+// 거주지구 ( 주민등록인구 > 생활인구 )
+export const COLOR_BLUE_SCALE = x =>
   // https://github.com/d3/d3-scale-chromatic
     (
       scaleSequential()
-      .domain([0, 40]) // 차이값의 최대가 40000미만이므로
+      .domain([0, 20]) // 차이값의 최대가 40000미만이므로
       .interpolator(interpolateBlues)
     )(x) // return a string color "rgb(R,G,B)"
     .slice(4,-1)  // extract "R,G,B"
     .split(',') // spline into an array ["R", "G", "B"]
     .map(x => parseInt(x,10));  // convert to [R, G, B]
 
+// 업무지구 ( 생활인구 > 주민등록인구 )
+export const COLOR_RED_SCALE = x =>
+  // https://github.com/d3/d3-scale-chromatic
+    (
+      scaleSequential()
+      .domain([0, 20]) // 차이값의 최대가 40000미만이므로
+      .interpolator(interpolateOrRd)
+    )(x) // return a string color "rgb(R,G,B)"
+    .slice(4,-1)  // extract "R,G,B"
+    .split(',') // spline into an array ["R", "G", "B"]
+    .map(x => parseInt(x,10));  // convert to [R, G, B]
+
+// 값에 따라 알맞은 색으로 분기
+export const DIV_COLOR_SCALE = function (isPositive, value){
+      if(isPositive){
+        return COLOR_BLUE_SCALE(value);
+      }else {
+        return COLOR_RED_SCALE(value);
+      }
+    }
 
 const INITIAL_VIEW_STATE = {
   // 서울시청 좌표
@@ -50,6 +69,7 @@ const dirLight = new SunLight({
   _shadow: false
 });
 
+
 function getTooltip({object}) {
   return (
     object && {
@@ -61,26 +81,6 @@ function getTooltip({object}) {
       `
     }
     );
-    // <div>|주민등록인구 - 생활인구| (내국인만): ${object.properties.diff_val.toLocaleString()}</div>
-    // return (
-  //   object && {
-  //     html: `\
-  //     <div><b>${object.properties.adm_nm}</b></div>
-  //     <div>총인구수: ${object.properties.population.total.toLocaleString()} 
-  //       (남 ${object.properties.population.total_m.toLocaleString()} / 
-  //       여 ${object.properties.population.total_f.toLocaleString()}) </div>
-  //     <div>내국인수: ${object.properties.population.citizens.toLocaleString()} 
-  //       (남 ${object.properties.population.citizens_m.toLocaleString()} / 
-  //       여 ${object.properties.population.citizens_f.toLocaleString()}) </div>
-  //     <div>외국인수: ${object.properties.population.foreigners.toLocaleString()} 
-  //       (남 ${object.properties.population.foreigners_m.toLocaleString()} / 
-  //       여 ${object.properties.population.foreigners_f.toLocaleString()}) </div>
-  //     <div>총세대수: ${object.properties.population.households.toLocaleString()} </div>
-  //     <div>세대당 인구: ${object.properties.population.per_household.toLocaleString()} </div>
-  //     <div>고령자(65세 이상): ${object.properties.population.seniors.toLocaleString()} </div>
-  // `
-  //   }
-  // );
 }
 
 export default function App({data = DATA_URL, mapStyle = 'mapbox://styles/mapbox/light-v9'}) {
@@ -113,8 +113,7 @@ export default function App({data = DATA_URL, mapStyle = 'mapbox://styles/mapbox
       extruded: true,
       wireframe: true,
       getElevation: f => f.properties.diff_val * 0.1, // 0.1 -> polygon 높이 조절
-      // getElevation: f => f.properties.population.total * 0.05, // 주민등록 인구수
-      getFillColor: f => COLOR_SCALE(f.properties.diff_val / 1000), // 1000 -> 색 범위 조절
+      getFillColor: f => DIV_COLOR_SCALE(f.properties.diff_bool, f.properties.diff_val / 2000), // 2000 -> 색 범위 조절
       getLineColor: [255, 255, 255],
       pickable: true
     })
@@ -170,18 +169,7 @@ export function renderToDOM(container) {
           let key = row[2].replaceAll(".","·"); 
 
           dict_population[key] = {
-            // total:parseIntComma(row[4]),  // 총인구수
-            // total_m:parseIntComma(row[5]),  // 남성인구수
-            // total_f:parseIntComma(row[6]),  // 여성인구수
             citizens:parseIntComma(row[7]), // 총내국인수
-            // citizens_m:parseIntComma(row[8]), // 남자내국인수
-            // citizens_f:parseIntComma(row[9]), // 여자내국인수
-            // foreigners:parseIntComma(row[10]), // 총외국인수
-            // foreigners_m:parseIntComma(row[11]), // 남자외국인수
-            // foreigners_f:parseIntComma(row[12]), // 여자외국인수
-            // households:parseIntComma(row[3]), // 세대수
-            // per_household:parseIntComma(row[13]), // 세대별 평균 인구수
-            // seniors:parseIntComma(row[14]),  // 고령자(65세 이상)
           }
       }
 
@@ -223,7 +211,7 @@ export function renderToDOM(container) {
         // 실 생활인구 데이터 추가 (행정동코드 10자리를 기준으로)
         let new_data_value = dict_real_population[f.properties.adm_cd2];
         if(!new_data_value){
-          console.log('adm_cd2 [' + f.properties.adm_cd2 + '] is Empty');
+          // console.log('adm_cd2 [' + f.properties.adm_cd2 + '] is Empty');
           // 결측치 처리 (주민등록 총 내국인 수로 채움)
           this[idx].properties.real_population = this[idx].properties.population;
         } else{
@@ -236,9 +224,11 @@ export function renderToDOM(container) {
         if(doc_total < real_total){ // 219 개
           this[idx].properties.diff_val = real_total - doc_total;
           this[idx].properties.diff_str = '실 생활인구가 많음 ( +';
+          this[idx].properties.diff_bool = false; // RED
         } else{ // 206 개
           this[idx].properties.diff_val = doc_total - real_total;
           this[idx].properties.diff_str = '실 생활인구가 적음 ( -';
+          this[idx].properties.diff_bool = true; // BLUE
         }
 
     }, filtered_features);
